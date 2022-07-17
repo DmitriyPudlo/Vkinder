990d9dd9fb9c5a49bfc052eae5768602626d902c
DmitriyPudlo <teinalandergor@gmail.com>
2022-07-17 21:17:01 +0500

    Add class Search. Need to implement photo search

--- vkinder_bot.py
+++ vkinder_bot.py
@@ -73,8 +73,8 @@ class Search(WorkWithGet):
 
 
 if __name__ == '__main__':
-    token_for_bot = 'vk1.a.JxoJoWFTReNOLAljFNSnro8hPf-id5kL96fZMNav2GpjCkRyq-nCjfzKZ7Qw0JGLaqvaGkCGxaQd9wu5gFbbe-lJ9SI8V1ylZAU8b5UtHKuyE7LYY7leev62QD56yu9fVwsmYRhSyPDevWqdjI3QwJwIEV_bHqEPQ8i_-Fh-W0OVYQBk4xuPHfDUq9CbMSFD'
-    token_for_get = 'vk1.a.91ZpDzsxgBpqsfabik3ZtJXTQmHr_NwlABJc3-NJwylWyWrHEF78HHofS5nTyFeKSe0JmmOU9oKjlBp9gceEjNJFSYYs6Qe25z65hTNUVfrvNLBZMgqHCrqz-nvAgMLZ5_4zLjBVgwfhKpEk7aBAh1-s0-Xq6dFygbEnP17tSAFRSDGmN5hJxqMtlLj3xPKR'
+    token_for_bot = ''
+    token_for_get = ''
     url_api_for_client = 'https://api.vk.com/method/users.get'
     url_api_for_search = 'https://api.vk.com/method/users.search'
     vk_bot = Bot(token_for_bot)
