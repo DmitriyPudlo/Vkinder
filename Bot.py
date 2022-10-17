96f4c9bb4083813f5bbf6c440d836a9ebfd53ea5
DmitriyPudlo <teinalandergor@gmail.com>
2022-10-17 21:35:51 +0500

    ОПтимизированы цикл и передача некоторых данных Optimized the cycle and transmission of some data

--- Bot.py
+++ Bot.py
@@ -6,11 +6,6 @@ import vk_api
 import Keyboard
 
 
-def to_list(tuple_):
-    list_ = [elem[0] for elem in tuple_]
-    return list_
-
-
 def prepare_info(candidate_id, response, photos_ids):
     address = {1: 'её', 2: 'его'}
     first_name = response['first_name']
@@ -27,18 +22,19 @@ class Bot:
     def __init__(self, token_for_bot, token_for_get):
         self.token_for_bot = token_for_bot
         self.token_for_get = token_for_get
-        self.vk = vk_api.VkApi(token=self.token_for_bot)
-        self.longpoll = VkLongPoll(self.vk)
-        self.get = VK(token_for_get)
+        self.vk_api = vk_api.VkApi(token=self.token_for_bot)
+        self.longpoll = VkLongPoll(self.vk_api)
+        self.vk = VK(token_for_get)
         self.connect = Connector()
-        self.connect.create_tables()
+        self.connect.create_database()
+        self.keyword = Keyboard.Keyword()
 
     def write_msg(self, user_id, message, attachment=None, keyboard=None):
-        self.vk.method('messages.send', {'user_id': user_id,
-                                         'message': message,
-                                         'random_id': randrange(10 ** 7),
-                                         'attachment': attachment,
-                                         'keyboard': keyboard})
+        self.vk_api.method('messages.send', {'user_id': user_id,
+                                             'message': message,
+                                             'random_id': randrange(10 ** 7),
+                                             'attachment': attachment,
+                                             'keyboard': keyboard})
 
     def greeting(self):
         for event in self.longpoll.listen():
@@ -52,7 +48,7 @@ class Bot:
             if event.type == VkEventType.MESSAGE_NEW:
                 if event.to_me:
                     request = event.text
-                    if request == "Старт":
+                    if request == self.keyword.START:
                         self.write_msg(event.user_id,
                                        "Сейчас я изучу твой профиль и постараюсь найти для тебя подходящего партнёра!")
                         return
@@ -62,25 +58,25 @@ class Bot:
             if event.type == VkEventType.MESSAGE_NEW:
                 if event.to_me:
                     request = event.text
-                    if request == "Следующий" or request == "Следующий из списка избранных":
+                    if request == self.keyword.NEXT or request == self.keyword.NEXT_FAVOR:
                         self.write_msg(event.user_id, "Сейчас поищу...")
                         return
-                    elif request == "Добавить в избранные":
+                    elif request == self.keyword.ADD_FAVOR:
                         self.connect.add_candidate(event.user_id, user_id)
                         for photo_id in photos_id:
                             self.connect.add_photo(user_id, photo_id)
                         keyboard = Keyboard.add_favor_key
                         self.write_msg(event.user_id, "Добавлен в ваш список избранных!", keyboard=keyboard)
                         return self.speak()
-                    elif request == "Показать избранных":
+                    elif request == self.keyword.SHOW_FAVOR:
                         self.write_msg(event.user_id, "Сейчас выведу ваших избранных")
-                        return "Показать избранных"
-                    elif request == 'Продолжить просмотр кандидатов':
+                        return self.keyword.SHOW_FAVOR
+                    elif request == self.keyword.CONTINUE:
                         self.write_msg(event.user_id, "Возвращаемся к просмотру кандидатов")
-                        return 'Продолжить просмотр кандидатов'
-                    elif request == "Стоп":
+                        return self.keyword.CONTINUE
+                    elif request == self.keyword.STOP:
                         self.write_msg(event.user_id, "Пока!")
-                        return "Стоп"
+                        return self.keyword.STOP
 
     def response(self, info):
         for event in self.longpoll.listen():
@@ -116,25 +112,24 @@ class Bot:
         self.connect.add_client(client_id)
         self.start()
         while True:
-            criteria_search = self.get.search_client_info(client_id)
-            candidates_ids = self.get.search_candidate(criteria_search)
+            candidates_ids = self.vk.search_candidate(client_id)
             for id_candidate in candidates_ids:
-                photos_ids = self.get.photos_ids(id_candidate)
-                response = self.get.show_candidate(id_candidate)
+                photos_ids = self.vk.photos_ids(id_candidate)
+                response = self.vk.show_candidate(id_candidate)
                 to_bot = prepare_info(id_candidate, response, photos_ids)
                 self.response(to_bot)
                 self.show_key(Keyboard.response_key)
                 command = self.speak(id_candidate, photos_ids)
-                if command == "Показать избранных":
+                if command == self.keyword.SHOW_FAVOR:
                     response_favor = Keyboard.response_favor_key
                     favor_ending = Keyboard.favor_ending_key
                     self.show_favor(response_favor, favor_ending, client_id)
-                elif command == "Стоп":
+                elif command == self.keyword.STOP:
                     break
             self.ending()
             self.show_key(Keyboard.ending_key)
             command = self.speak()
-            if command == "Показать избранных":
+            if command == self.keyword.SHOW_FAVOR:
                 favor_without_candidates = Keyboard.response_favor_without_candidates_key
                 favor_ending_without_candidates = Keyboard.favor_ending_without_candidates_key
                 self.show_favor(favor_without_candidates, favor_ending_without_candidates, client_id)
@@ -143,16 +138,14 @@ class Bot:
 
     def show_favor(self, key_begin, key_end, client_id):
         favor_ids = self.connect.show_candidates(client_id)
-        favor_ids = to_list(favor_ids)
         for favor_id in favor_ids:
             favor_photos_ids = self.connect.show_photo(favor_id)
-            favor_photos_ids = to_list(favor_photos_ids)
-            response = self.get.show_candidate(favor_id)
+            response = self.vk.show_candidate(favor_id)
             favor_to_bot = prepare_info(favor_id, response, favor_photos_ids)
             self.response_favor(favor_to_bot)
             self.show_key(key_begin)
             answer = self.speak()
-            if answer == 'Продолжить просмотр кандидатов':
+            if answer == self.keyword.CONTINUE:
                 return
         self.favor_ending()
         self.show_key(key_end)
