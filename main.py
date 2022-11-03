cf4a5f9c428a9001e13001f4f11946f5ae8bf21e
DmitriyPudlo <teinalandergor@gmail.com>
2022-11-03 14:06:05 +0500

    two process?

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     token_for_get = f'{tokens.GET_TOKEN}'
     print('START')
     vk_bot = Bot(token_for_bot, token_for_get)
-    app.run(debug=True)
-    # vk_bot.bot_start()
+    # app.run(debug=True)
+    vk_bot.bot_start()
 
