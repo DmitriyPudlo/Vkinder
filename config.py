556e292215e8ed769ad3eab292c070b1384d780f
DmitriyPudlo <teinalandergor@gmail.com>
2022-11-19 16:28:35 +0500

    add work with Telegram

---
+++ config.py
@@ -0,0 +1,6 @@
+import Keyboard
+
+
+FAVOR_COMMANDS = {False: (Keyboard.response_favor_without_candidates_key, Keyboard.favor_ending_without_candidates_key),
+                  True: (Keyboard.response_favor_key, Keyboard.favor_ending_key)}
+ADDRESS = {1: 'её', 2: 'его'}
