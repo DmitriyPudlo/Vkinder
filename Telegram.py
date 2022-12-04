0e88dcdbd00ad04903c52737a19f31eb7bd7bfef
DmitriyPudlo <teinalandergor@gmail.com>
2022-12-04 15:56:37 +0500

    add work with Telegram

--- Telegram.py
+++ Telegram.py
@@ -6,25 +6,8 @@ import Keyboard
 from db import Connector
 import config
 
-token_for_get = f'{tokens.GET_TOKEN}'
 token_for_tele = f'{tokens.TELEGRAM_TOKEN}'
 tele_bot = telebot.TeleBot(token_for_tele)
-keyword = Keyboard.Keyword()
-
-
-class Telegram:
-    def __init__(self, token):
-        self.token = token
-        self.tele_bot = telebot.TeleBot(token_for_tele)
-        self.is_end_dialog = True
-
-    def flat_generator(self, lists):
-        cnt1 = 0
-        while len(lists) > cnt1:
-            if len(lists) == cnt1:
-                self.is_end_dialog = False
-            yield lists[cnt1]
-            cnt1 += 1
 
 
 def create_markup(keyboard):
@@ -34,68 +17,76 @@ def create_markup(keyboard):
     return markup
 
 
-def prepare_info(candidate_id):
-    response = vk.show_candidate(candidate_id)
-    photos_ids = vk.photos_ids(candidate_id)
-    first_name = response['first_name']
-    last_name = response['last_name']
-    url = f'https://vk.com/id{candidate_id}'
-    photos_url = [f'https://vk.com/photo{candidate_id}_{photo_id}' for photo_id in photos_ids]
-    photos_url = ', '.join(photos_url)
-    info = f'''Имя: {first_name} {last_name},
-Ссылка на её профиль: {url},
-Самые популярные фотографии: {photos_url}'''
-    return info
+class Telegram:
+    def __init__(self, token):
+        self.token = token
+        self.is_end_dialog = True
+        self.vk = VK(self.token)
+        self.connect = Connector()
+        self.keyword = Keyboard.Keyword()
+        self.user_id = self.vk.get_user_id()
+        self.id_candidates = self.vk.search_candidate(self.user_id)
+        self.favor_ids = self.connect.show_candidates(self.user_id)
+        self.candidate_generator = self.generator(self.id_candidates)
+        self.favor_generator = self.generator(self.favor_ids)
 
+    def generator(self, lists):
+        cnt1 = 0
+        while len(lists) > cnt1:
+            if len(lists) == cnt1:
+                self.is_end_dialog = False
+            yield lists[cnt1]
+            cnt1 += 1
 
-@tele_bot.message_handler(commands=['start'])
-def start(message):
-    tele_bot.send_message(message.chat.id,
-                          "Привет! Я помогу тебе найти партнера для знакомств!")
-    markup = create_markup(Keyboard.greeting)
-    tele_bot.send_message(message.chat.id, 'Для продолжения нажми кнопку ниже!', reply_markup=markup)
+    def prepare_info(self, candidate_id):
+        response = self.vk.show_candidate(candidate_id)
+        photos_ids = self.vk.photos_ids(candidate_id)
+        first_name = response['first_name']
+        last_name = response['last_name']
+        url = f'https://vk.com/id{candidate_id}'
+        photos_url = [f'https://vk.com/photo{candidate_id}_{photo_id}' for photo_id in photos_ids]
+        photos_url = ', '.join(photos_url)
+        info = f'''Имя: {first_name} {last_name},
+    Ссылка на её профиль: {url},
+    Самые популярные фотографии: {photos_url}'''
+        return info
 
-
-@tele_bot.message_handler(content_types='text')
-def speak(message):
-    candidate_id = ''
-    if message.text == keyword.START or message.text == keyword.NEXT or message.text == keyword.CONTINUE:
-        if message.text == keyword.CONTINUE:
-            tele_bot.send_message(message.chat.id,
-                                  'Возвращаемся к просмотру кандидатов')
-        else:
-            tele_bot.send_message(message.chat.id,
-                                  "Сейчас поищу...")
-        candidate_id = next(candidate_generator)
-        to_bot = prepare_info(candidate_id)
-        tele_bot.send_message(message.chat.id, f'{to_bot}')
+    @tele_bot.message_handler(commands=['start'])
+    def start(self, message):
         tele_bot.send_message(message.chat.id,
-                              '____________________')
-        markup = create_markup(Keyboard.response)
+                              "Привет! Я помогу тебе найти партнера для знакомств!")
+        markup = create_markup(Keyboard.greeting)
         tele_bot.send_message(message.chat.id, 'Для продолжения нажми кнопку ниже!', reply_markup=markup)
-    if message.text == keyword.STOP:
-        tele_bot.send_message(message.chat.id, "Пока")
-        return
-    if message.text == keyword.ADD_FAVOR:
-        photos_ids = vk.photos_ids(candidate_id)
-        connect.add_candidate(user_id, candidate_id, photos_ids)
-        markup = create_markup(Keyboard.add_favor_key)
-        tele_bot.send_message(message.chat.id, "Добавлен в ваш список избранных!", reply_markup=markup)
-    if message.text == keyword.SHOW_FAVOR or message.text == keyword.NEXT_FAVOR:
-        favor_id = next(favor_generator)
-        favor_to_bot = prepare_info(favor_id)
-        markup = create_markup(config.FAVOR_COMMANDS[telegram.is_end_dialog])
-        if message.text == keyword.SHOW_FAVOR:
-            tele_bot.send_message(message.chat.id, "Сейчас выведу ваших избранных", reply_markup=markup)
-        tele_bot.send_message(message.chat.id, f'{favor_to_bot}', reply_markup=markup)
 
-
-vk = VK(token_for_get)
-user_id = vk.get_user_id()
-telegram = Telegram(token_for_get)
-id_candidates = vk.search_candidate(user_id)
-candidate_generator = telegram.flat_generator(id_candidates)
-connect = Connector()
-favor_ids = connect.show_candidates(user_id)
-favor_generator = telegram.flat_generator(favor_ids)
-tele_bot.polling(none_stop=True)
+    @tele_bot.message_handler(content_types='text')
+    def speak(self, message):
+        candidate_id = ''
+        if message.text == self.keyword.START or message.text == self.keyword.NEXT or message.text == self.keyword.CONTINUE:
+            if message.text == self.keyword.CONTINUE:
+                tele_bot.send_message(message.chat.id,
+                                      'Возвращаемся к просмотру кандидатов')
+            else:
+                tele_bot.send_message(message.chat.id,
+                                      "Сейчас поищу...")
+            candidate_id = next(self.candidate_generator)
+            to_bot = self.prepare_info(candidate_id)
+            tele_bot.send_message(message.chat.id, f'{to_bot}')
+            tele_bot.send_message(message.chat.id,
+                                  '____________________')
+            markup = create_markup(Keyboard.response)
+            tele_bot.send_message(message.chat.id, 'Для продолжения нажми кнопку ниже!', reply_markup=markup)
+        if message.text == self.keyword.STOP:
+            tele_bot.send_message(message.chat.id, "Пока")
+            return
+        if message.text == self.keyword.ADD_FAVOR:
+            photos_ids = self.vk.photos_ids(candidate_id)
+            self.connect.add_candidate(self.user_id, candidate_id, photos_ids)
+            markup = create_markup(Keyboard.add_favor_key)
+            tele_bot.send_message(message.chat.id, "Добавлен в ваш список избранных!", reply_markup=markup)
+        if message.text == self.keyword.SHOW_FAVOR or message.text == self.keyword.NEXT_FAVOR:
+            favor_id = next(self.favor_generator)
+            favor_to_bot = self.prepare_info(favor_id)
+            markup = create_markup(config.FAVOR_COMMANDS[self.is_end_dialog])
+            if message.text == self.keyword.SHOW_FAVOR:
+                tele_bot.send_message(message.chat.id, "Сейчас выведу ваших избранных", reply_markup=markup)
+            tele_bot.send_message(message.chat.id, f'{favor_to_bot}', reply_markup=markup)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Bot import Bot
 from API import app
+from Telegram import Telegram, tele_bot
 import tokens
 
 
@@ -8,6 +9,9 @@ if __name__ == '__main__':
     token_for_get = f'{tokens.GET_TOKEN}'
     print('START')
     vk_bot = Bot(token_for_bot, token_for_get)
-    # app.run(debug=True)
+    telegram = Telegram(token_for_get)
     vk_bot.bot_start()
+    app.run(debug=True)
+    tele_bot.polling(none_stop=True)
+
 
